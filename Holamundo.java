61d98d6c53b0eb2dab332a3327a6cc13abf92fe9
jgimenezbalmes <jgimenez@jaumebalmes.net>
2022-09-30 17:07:10 +0200

    cambio joan

--- Holamundo.java
+++ Holamundo.java
@@ -4,7 +4,7 @@ package holamundo;
 public class Holamundo {
 
     public static String holamundo(){
-        return "hola mundo";
+        return "hola mundo, me llamo Joan";
     } 
     public static void main(String[] args) {
         System.out.println(holamundo());
