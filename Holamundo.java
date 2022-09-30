524f47f8f92c584d0e8f45b79662122018814426
JonathanValleA <jvalle@jaumebalmes.net>
2022-09-30 16:57:42 +0200

    Hola mundo

---
+++ Holamundo.java
@@ -0,0 +1,12 @@
+
+package holamundo;
+
+public class Holamundo {
+
+    public static String holamundo(){
+        return "hola mundo";
+    } 
+    public static void main(String[] args) {
+        System.out.println(holamundo());
+    }   
+}
