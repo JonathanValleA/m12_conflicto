6a07e2302a45bd4966e29318b52f953b4f865c02
JonathanValleA <jvalle@jaumebalmes.net>
2022-09-30 17:08:08 +0200

    otro cambio

--- Holamundo.java
+++ Holamundo.java
@@ -4,7 +4,7 @@ package holamundo;
 public class Holamundo {
 
     public static String holamundo(){
-        return "hola mundo";
+        return "cambio jonathan";
     } 
     public static void main(String[] args) {
         System.out.println(holamundo());
